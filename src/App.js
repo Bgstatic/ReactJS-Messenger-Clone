4ec6ed53e59645e57e5cf76d0b6c63104a96b90e
Bgstatic <bilgehangecici@gmail.com>
2020-09-12 15:33:43 +0300

    Added message section and send button.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [message, setMessage] = useState([])
+  const sendMessage = (event) => {
+     
+    setMessage([...message, input])
+    setInput('');
+
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ready to Go</h1>
+      <input type="text" value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
