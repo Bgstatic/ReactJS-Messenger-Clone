5313342000c3f14d2ea37dc2657cd3d840d2dcaa
Bgstatic <bilgehangecici@gmail.com>
2020-09-12 21:29:11 +0300

    Firebase updated.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   useEffect(() => {
 
-    db.collection('messages').orderBy('timestamp', 'asc').onSnapshot(snapshot => {
+    db.collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
 
     });
